99ee08e6bd69291e055ce9e86828373b8901249d
Fabian Leonardo Ortiz Cuevas <fabioleorcu20@gmail.com>
2025-12-24 10:25:46 -0500

    Feat : add function standardize_dates an parser library

--- sample_project/preprocess.py
+++ sample_project/preprocess.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import numpy as np
+from dateutil import parser
 
 def advanced_cleaning_with_pandas(data_list):
     """Convierte una lista a Serie de Pandas para eliminar duplicados y nulos rápidamente."""
@@ -11,3 +12,13 @@ def normalize_scales(numbers):
     arr = np.array(numbers)
     if arr.size == 0: return []
     return ((arr - arr.min()) / (arr.max() - arr.min())).tolist()
+
+def standardize_dates(date_strings):
+    """Usa python-dateutil para convertir diversos formatos de fecha a ISO estándar."""
+    standard_dates = []
+    for date in date_strings:
+        try:
+            standard_dates.append(parser.parse(date).isoformat())
+        except:
+            continue
+    return standard_dates
